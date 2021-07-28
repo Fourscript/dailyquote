c41e03204da811784c1dd6d7787915a8660c1a94
Richard M <70453767+MorandR@users.noreply.github.com>
2021-07-27 22:55:38 -0400

    Finalized Data Retrieval
    Fetching data from different api to extract formatted quote to simplified code.
    
    Co-Authored-By: Tevin Rose <23547919+Blarsu@users.noreply.github.com>
    Co-Authored-By: Evan C. Navarro <EvanCNavarro@gmail.com>
    Co-Authored-By: danteedmonson <74576480+danteedmonson@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
-// window.onload();
+//URL Reference for object data retrieval: https://theysaidso.com/api/#jod-json
 
-let quote = "";
-// document.querySelector("#quote").innerHTML= quote;
+fetch("https://quotes.rest/qod?category=inspire")
+    .then(res => res.json())
+    .then(data => {
+        let quote = data.contents.quotes[0].quote;
+        let author = data.contents.quotes[0].author;
+        let date = parseInt(data.contents.quotes[0].date, "-");
 
-
-let key = "fa09959d7acc43eb933b3aa8f2226770f8274f12";
-
-function get_quote_of_the_day() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-     if (this.readyState == 4 && this.status == 200) {
-         // Access the result here
-         alert(this.responseText);
-     }
-    };
-    xhttp.open("GET", "https://quotes.rest/qod?category=inspire", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.setRequestHeader("X-Theysaidso-Api-Secret", "YOUR API HERE");
-    xhttp.send();
-}
-
-get_quote_of_the_day();
-
-// function getQuote() {
-//     return fetch(`https://zenquotes.io/api/today/${key}`)
-//       .then(res => {
-//         return res.json()
-//       })
-//       .then(data => {
-//         return data[0]["q"] + " -" + data[0]["a"]
-//       })
-//   }
-
-
-//   getQuote();
-// const api_url ="https://quotes.rest/";
-
-// async function getapi(url)
-// {
-//   const response = await fetch(url);
-//   var data = await response.json();
-//   console.log(data);
-// }
-
-// getapi(api_url);
-
-// fetch(`https://zenquotes.io/api/today/${key}`)
-//     .then(res=>res.json())
-//     .then(data =>console.log(data + "cheese")
-//     );
-
-//     console.log("bread");
+        let qod = `"${quote}" - ${author}, ${date}`;
+        document.querySelector("#quote").innerHTML = qod;
+    }
+    );
